65ddcc205e01fa3b59c62261acba964e215a8397
karljay98 <karljace@msn.com>
2022-10-31 04:08:19 +0000

    first table created

--- college_database.sql
+++ college_database.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE college_library;
 USE college_library;
+
+CREATE TABLE liberian (
+	liberian_id INT NOT NULL, 
+	hours FLOAT, 
+	date_of_joining DATE,
+    PRIMARY KEY (liberian_id)
+);
